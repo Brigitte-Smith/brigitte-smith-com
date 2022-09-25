da97e445f6cabb69150a07708ae319d8d4b06d17
absolutholz <absolutholz@gmail.com>
2022-09-25 16:28:27 +0200

    Add setup files

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	i18n: {
+		locales: ["en", "de"],
+		defaultLocale: "de",
+		localeDetection: false,
+	},
+	trailingSlash: true,
+	compiler: {
+		styledComponents: true,
+	},
+};
+
+module.exports = nextConfig;
