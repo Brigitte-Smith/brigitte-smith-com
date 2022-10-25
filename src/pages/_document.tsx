798f4879fb2c189552e0095b15fd8ee8c694a691
absolutholz <absolutholz@gmail.com>
2022-10-25 06:07:40 +0200

    Update ssr styled components generation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,46 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-function Document({ __NEXT_DATA__ }: any) {
-	return (
-		<Html lang={__NEXT_DATA__.page.startsWith("/en") ? "en" : "de"}>
-			<Head></Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			const { pathname } = ctx;
+			const locale = pathname.startsWith("/de") ? "de" : "en";
+
+			return {
+				...initialProps,
+				locale,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
+
+	render() {
+		return (
+			<Html lang={this.props.locale}>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
-
-export default Document;
