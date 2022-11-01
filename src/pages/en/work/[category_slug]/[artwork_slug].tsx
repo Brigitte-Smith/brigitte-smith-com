b250cbea00c353fbd76eb9a8912de90e130c277b
absolutholz <absolutholz@gmail.com>
2022-11-01 10:18:31 +0100

    Add artwork microdata

--- src/pages/en/work/[category_slug]/[artwork_slug].tsx
+++ src/pages/en/work/[category_slug]/[artwork_slug].tsx
@@ -20,6 +20,7 @@ import {
 } from "../../../../lib/artworkPage";
 import { ArtworkStatistic } from "../../../../components/ArtworkStatistic";
 import topLevel from "../../../../../data/topLevel.json";
+import Head from "next/head";
 
 // dirty way to render static pages without next's script files
 export const config = {
@@ -83,84 +84,124 @@ export default function ArtworkPage({
 	const nextArtworkSlug = nextArtwork?.slug;
 	const nextArtworkTitle = nextArtwork?.title;
 
+	const jsonLD = {
+		"@context": "https://schema.org",
+		"@type": "VisualArtwork",
+		name: artworkName,
+		artist: "Brigitte Smith",
+		image: `http://www.brigitte-smith.com/images/artwork/${image}`,
+	};
+
+	if (artworkDate) {
+		jsonLD["dateCreated"] = artworkDate;
+	}
+
+	if (artworkSize) {
+		jsonLD["width"] = [
+			{
+				"@type": "Distance",
+				name: `${artwork.size?.width.value} ${artwork.size?.width.unit}`,
+			},
+		];
+		jsonLD["height"] = [
+			{
+				"@type": "Distance",
+				name: `${artwork.size?.height.value} ${artwork.size?.height.unit}`,
+			},
+		];
+	}
+
 	return (
-		<FrameLayout>
-			<MetaTitle suffix={artworkName} />
-			<ArtworkColumns
-				slotImage={
-					<Link href={`/images/artwork/${image}`}>
-						<a>
-							<picture>
-								<source
-									srcSet={`/images/artwork/380/${image}`.replace(
-										/.(jpe?g|png)$/i,
-										".webp"
-									)}
-									type="image/webp"
-								/>
-								<ArtworkImage
-									alt={artworkName}
-									src={`/images/artwork/380/${image}`}
-								/>
-							</picture>
-						</a>
-					</Link>
-				}
-				slotContent={
-					<>
-						<div>
-							<h1>{artworkName}</h1>
-							{artworkSize && (
-								<ArtworkStatistic label="size">
-									{artworkSize}
-								</ArtworkStatistic>
+		<>
+			<Head>
+				<script type="application/ld+json">
+					{JSON.stringify(jsonLD)}
+				</script>
+			</Head>
+			<FrameLayout>
+				<MetaTitle suffix={artworkName} />
+				<ArtworkColumns
+					slotImage={
+						<Link href={`/images/artwork/${image}`}>
+							<a>
+								<picture>
+									<source
+										srcSet={`/images/artwork/380/${image}`.replace(
+											/.(jpe?g|png)$/i,
+											".webp"
+										)}
+										type="image/webp"
+									/>
+									<ArtworkImage
+										alt={artworkName}
+										src={`/images/artwork/380/${image}`}
+									/>
+								</picture>
+							</a>
+						</Link>
+					}
+					slotContent={
+						<>
+							<div>
+								<h1>{artworkName}</h1>
+								{artworkSize && (
+									<ArtworkStatistic label="size">
+										{artworkSize}
+									</ArtworkStatistic>
+								)}
+								{artworkDate && (
+									<ArtworkStatistic label="year">
+										{
+											<time>
+												{artworkDate.getFullYear()}
+											</time>
+										}
+									</ArtworkStatistic>
+								)}
+							</div>
+							<div>
+								{artworkIndex + 1}/{artworkCount}
+							</div>
+						</>
+					}
+					slotNav={
+						<>
+							{previousArtwork ? (
+								<Link
+									href={`/${locale}/${topLevel.artwork[locale].data.slug}/${categorySlug}/${previousArtworkSlug}`}
+								>
+									<a
+										aria-label={`go to ${previousArtworkTitle}`}
+									>
+										<SvgIcon aria-hidden="true">
+											<path
+												fill="currentColor"
+												d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z"
+											/>
+										</SvgIcon>
+									</a>
+								</Link>
+							) : (
+								<span />
 							)}
-							{artworkDate && (
-								<ArtworkStatistic label="year">
-									{<time>{artworkDate.getFullYear()}</time>}
-								</ArtworkStatistic>
+							{nextArtwork && (
+								<Link
+									href={`/${locale}/${topLevel.artwork[locale].data.slug}/${categorySlug}/${nextArtworkSlug}`}
+								>
+									<a aria-label={`go to ${nextArtworkTitle}`}>
+										<SvgIcon aria-hidden="true">
+											<path
+												fill="currentColor"
+												d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
+											/>
+										</SvgIcon>
+									</a>
+								</Link>
 							)}
-						</div>
-						<div>
-							{artworkIndex + 1}/{artworkCount}
-						</div>
-					</>
-				}
-				slotNav={
-					<>
-						{previousArtwork ? (
-							<Link
-								href={`/${locale}/${topLevel.artwork[locale].data.slug}/${categorySlug}/${previousArtworkSlug}`}
-							>
-								<a aria-label={`go to ${previousArtworkTitle}`}>
-									<SvgIcon aria-hidden="true">
-										<path
-											fill="currentColor"
-											d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z"
-										/>
-									</SvgIcon>
-								</a>
-							</Link>
-						) : (
-							<span />
-						)}
-						{nextArtwork && (
-							<Link
-								href={`/${locale}/${topLevel.artwork[locale].data.slug}/${categorySlug}/${nextArtworkSlug}`}
-							>
-								<a aria-label={`go to ${nextArtworkTitle}`}>
-									<SvgIcon aria-hidden="true">
-										<path
-											fill="currentColor"
-											d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
-										/>
-									</SvgIcon>
-								</a>
-							</Link>
-						)}
-					</>
-				}
-			/>
-		</FrameLayout>
+						</>
+					}
+				/>
+			</FrameLayout>
+		</>
 	);
 }
