64d6a271fb29f6b0a4e77056ca65924f2222ebd5
absolutholz <absolutholz@gmail.com>
2022-11-01 11:18:35 +0100

    Add visually hidden component

---
+++ src/components/VisuallyHidden/VisuallyHidden.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const VisuallyHidden = styled.div`
+	border: 0;
+	clip: rect(0 0 0 0);
+	height: auto;
+	margin: 0;
+	overflow: hidden;
+	padding: 0;
+	position: absolute;
+	white-space: nowrap;
+	width: 1px;
+`;
